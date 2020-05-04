48c430027ac641bfef0370ef7af0ab5602a147e7
salikovskiy <ssalikovskiy@gmail.com>
2020-05-04 11:19:02 +0300

    tree js basic setup done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Coin from "./components/Coin";
+import { Canvas } from "react-three-fiber";
+import Coin3d from "./components/3dCoin";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <Coin />
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Coin3d />
+      </Canvas>
     </>
   );
 }
